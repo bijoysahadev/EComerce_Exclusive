a9ab2ae431e12f226c57ecfd7b6492ec4b4da460
bijoysahadev <bijoysahamernstackdev@gmail.com>
2026-06-25 16:06:56 +0600

    feat: login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,40 @@ import React from 'react'
 import Navbar from '../layouts/Navbar'
 import Footer from '../layouts/Footer'
 import NewsPart from '../layouts/NewsPart'
-
+import Container from '../Components/Container'
+import Image from '../Components/Image'
+import Login1 from '../assets/login.png'
+import Flex from '../Components/Flex'
+import Heading from '../Components/Heading'
+import { Form } from 'react-router-dom'
+import Button from '../Components/Button'
 const Login = () => {
   return (
-    <div>
-       <h1 className='text-9xl' >Login</h1>
-       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex rem voluptas laudantium quos eveniet reprehenderit nihil nesciunt veniam sit illum quae iure, deleniti, ullam iste fugiat repellat placeat dicta itaque eligendi cum nobis dignissimos perferendis quam. Asperiores unde molestiae delectus doloribus mollitia nisi dolores, ea aliquam ut voluptas laudantium? Corrupti nobis sunt quos. Sapiente, exercitationem. Eveniet impedit veniam accusantium nemo delectus nobis incidunt nulla, totam voluptatibus sapiente nisi quos ullam est dicta provident at aliquam aliquid consequatur a! Laudantium natus vel repudiandae reprehenderit ea quo amet reiciendis ipsam molestiae, laboriosam exercitationem magnam aspernatur alias quae quos velit quas. Nihil, ipsum!</p>
-    </div>
+    <>
+
+      <section className='py-[140px]' >
+         <Container>
+       <Flex className={` gap-x-[130px] justify-evenly items-center`} >
+       <div>    <Image src={Login1} /></div>
+       <div>
+         <Heading text='Log in to Exclusive' className={`font-medium`} />
+          <p  className='font-poppins text-[16px]   font-normal text-[rgb(0,0,0)] pt-5 pb-[40px]' > Enter your details below</p>
+          <Form>
+            <input  className='w-full outline-0 border-b-2 border-[rgba(0,0,0,0.10)]' type="text" placeholder='Email or Phone Number' />
+            <input  className='w-full outline-0 border-b-2 border-[rgba(0,0,0,0.10)] pt-16' type="text" placeholder='Password'   />
+          </Form>
+         <Flex className={`items-center justify-around pt-[50px]`}>
+                <Button text={`Log In`}/>
+                <p className='text-poppins text-red  text-4 font-normal' >Forget Password?</p>
+         </Flex>
+       </div>
+      
+       </Flex>
+     
+    </Container>
+      </section>
+   
+    </>
   )
 }
 
