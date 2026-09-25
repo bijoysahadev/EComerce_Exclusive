6356426bb9618ea3477c811605b9ec72a8926e1f
bijoysahadev <bijoysahamernstackdev@gmail.com>
2026-09-26 03:35:00 +0600

    feat::add proeduct cetalis page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -7,6 +7,8 @@ import Play2 from '../assets/play2.png'
 import Play3 from '../assets/play3.png'
 import Play4 from '../assets/play4.png'
 import PlayM from '../assets/playM.png'
+import { FaStar } from "react-icons/fa6";
+import { CiStar } from "react-icons/ci";
 const ProductDetails = () => {
   return (
     <section className='pt-20' >
@@ -38,8 +40,36 @@ const ProductDetails = () => {
         <div className='w-5/12  bg-amber-700' >
         <Image src={PlayM} />
         </div>
-        <div className='w-5/12 flex flex-col items-center  bg-blue-700' >
-        <p>Havic HV G-92 Gamepad</p></div>
+        <div className='w-5/12 flex flex-col items-  bg-blue-700' >
+        <p className=' ml-5 font-inter text-black font-semibold  text-[24px] leading-[24px] ' >Havic HV G-92 Gamepad</p>
+        <div className=' ml-4  flex gap-4 w-full flex mt-4 ' >
+       <div className='flex  text-yellow-500 ' >
+        <FaStar />
+   <FaStar />
+   <FaStar />
+   <FaStar />
+  <div className='text-black ' >
+    <CiStar />
+  </div>
+       </div>
+
+<div  className=  '  flex items-center gap-2 justify-center  font-poppins  font-normal text-[14px] text-[rgba(0,0,0,0.10)] ' >
+
+ <span>(</span><p>(150 Reviews)</p><span>)</span>  <span>  /</span>
+
+</div>
+<div className='' >
+    <p className='text-[#00FF66] font-poppins  font-normal text-[14px] ' >In Stock</p>
+</div>
+
+
+        </div >
+   
+      <h2  className={`ml-5  py-[10px] font-inter font-normal leading-[24px]  text-[#000000]   text-[24px] `} >$192.00</h2>
+      <div className='border-b border-black py-[24px]  ' >
+          <p className=' ml-5 font-poppins  font-normal  leading-[21px] text-[#000000] text-[14px] ' >PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.</p>
+      </div>
+        </div>
        
       </Flex>
     </Container>
